7a415f951f8a44d7127f8fcd292feb2fd0ff36ba
Credrido <156992589+Credrido@users.noreply.github.com>
2024-02-08 10:39:33 +0300

    Add files via upload

---
+++ ArrayBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal interface ArrayBase
+    {
+        void Createarray(bool randif);
+
+        void Getmiddle();
+
+        void Fillarray();
+
+        void Fillarrayrandomly();
+    }
+}
